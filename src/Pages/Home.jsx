31e0f5625caa1a68feba7d687d1b34f1c97de0b3
parasojha258 <parasojha258@gmail.com>
2019-09-23 15:06:50 +0545

    all pages added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import Services from '../components/Services';
+
+export default function Home() {
+    return (
+        <React.Fragment>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                    <Link to="/rooms" className="btn-primary">Our Rooms</Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </React.Fragment>
+    )
+}
